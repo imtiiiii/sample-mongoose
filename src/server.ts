294541d30539adcb93c1210ff8a59cd8e350fb99
imtiaz <imtiazahmed026@gmail.com>
2024-03-07 01:44:14 -0600

    setting up express

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,15 @@
-const str: string = "Hello World ts!";
-console.log(str);
+import express, { Application } from "express";
+import cors from "cors";
+
+const app: Application = express();
+const port: number = 3333;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
