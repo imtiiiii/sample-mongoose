7361c1e319609e8158a90b4e138dd84d760df81e
imtiaz <imtiazahmed026@gmail.com>
2024-03-08 00:11:46 -0600

    save user to db

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getUsersService } from "./user.service";
+import { createUserService, getUsersService } from "./user.service";
 import { IUser } from "./user.interface";
 
 export const getUsers = async (req: Request, res: Response) => {
@@ -7,4 +7,8 @@ export const getUsers = async (req: Request, res: Response) => {
   res.status(200).send({ message: "ok", data: users });
 };
 export const getUser = async (req: Request, res: Response) => {};
-export const createUser = async (req: Request, res: Response) => {};
+export const createUser = async (req: Request, res: Response) => {
+  const userInfo = req.body;
+  const createUser: IUser = await createUserService(userInfo);
+  res.status(201).send({ message: "success", data: createUser });
+};
--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -5,3 +5,8 @@ export const getUsersService = async (): Promise<IUser[]> => {
   const users = await User.find();
   return users;
 };
+export const createUserService = async (userInfo: IUser): Promise<IUser> => {
+  const user = new User(userInfo);
+  const newUser = await user.save();
+  return newUser;
+};
