0c8d27b1e191a988871fb78c0e51d6c4ea4f5dbe
imtiaz <imtiazahmed026@gmail.com>
2024-03-21 19:14:30 -0500

    Refactor user controller to validate user ID and handle errors

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -12,18 +12,28 @@ export const getUsers = async (req: Request, res: Response) => {
   res.status(200).send({ message: "ok", data: users });
 };
 export const getUser = async (req: Request, res: Response) => {
-  const { id } = req.params;
+  const { id } = req.params; // Extracting the user ID from request parameters
+
   try {
+    // Validating user ID using mongoose's Types.ObjectId
     const validUserIdObject = new mongoose.Types.ObjectId(id);
+
+    // Calling a service function to get the user by ID
     const findUser: IUser | null = await getUserService(validUserIdObject);
+
+    // If no user is found, throw an error
     if (!findUser) {
       throw new Error("invalid id");
     }
+
+    // Responding with status 200 and the user data if found
     res.status(200).send({ message: "ok", data: findUser });
   } catch (error) {
+    // Catching any errors and responding with status 400
     return res.status(400).send({ message: "Not found" });
   }
 };
+
 export const createUser = async (req: Request, res: Response) => {
   const userInfo = req.body;
   const createUser: IUser = await createUserService(userInfo);
