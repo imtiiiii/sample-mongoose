5efcf84b1dd13d17d30c07fc6ae1f759b2c71a50
imtiaz <imtiazahmed026@gmail.com>
2024-03-21 19:08:23 -0500

    Remove commented out code and unnecessary code in server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,34 +12,6 @@ const boot = async () => {
   }
 };
 boot();
-
-// interface IUser {
-//   firstName: string;
-//   lastName: string;
-// }
-// const userSchema = new mongoose.Schema<IUser>({
-//   firstName: {
-//     type: String,
-//     required: true,
-//   },
-//   lastName: {
-//     type: String,
-//     required: true,
-//   },
-// });
-// const User = mongoose.model<IUser>("User", userSchema);
-// const create = new User({
-//   firstName: "John",
-//   lastName: "Doe",
-// });
-// await create.save();
-// res.send(create);
-// const user = await new User({
-//   firstName: "John",
-//   lastName: "Doe",
-// });
-// await user.save();
-// res.status(200).json({ message: "success", data: user });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
